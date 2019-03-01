8c1e79740b7fd974a9aec4a1767e0ae65ab06cfe
José Joaquín Atria <jjatria@gmail.com>
2019-03-01 19:27:45 +0000

    Add support for TSX files through parsefile method for Maps and TileSets

---
+++ eg/split-towers.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="towers" tilewidth="25" tileheight="25" tilecount="12" columns="4">
+  <image source="towers.png" width="100" height="75"/>
+  <tile id="0">
+   <properties>
+    <property name="test_tower" value="1"/>
+   </properties>
+  </tile>
+</tileset>
